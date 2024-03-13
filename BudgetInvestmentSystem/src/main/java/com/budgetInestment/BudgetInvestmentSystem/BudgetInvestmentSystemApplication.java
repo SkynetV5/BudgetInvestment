ae1f3b4386f49bd777e1d0b46f168d12b898d517
Kamil <kjodlowski02@wp.pl>
2024-03-13 21:02:16 +0100

    inital commit

---
+++ BudgetInvestmentSystem/src/main/java/com/budgetInestment/BudgetInvestmentSystem/BudgetInvestmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.budgetInestment.BudgetInvestmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetInvestmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetInvestmentSystemApplication.class, args);
+	}
+
+}
---
+++ BudgetInvestmentSystem/src/test/java/com/budgetInestment/BudgetInvestmentSystem/BudgetInvestmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetInestment.BudgetInvestmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetInvestmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
