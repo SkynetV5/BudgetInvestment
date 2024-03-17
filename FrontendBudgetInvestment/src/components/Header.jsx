5678e2d09df8888662ac90dd2bee229d63174cd7
Kamil <kjodlowski02@wp.pl>
2024-03-17 18:17:37 +0100

    Dodanie loga do strony Login.jsx

--- FrontendBudgetInvestment/src/components/Header.jsx
+++ FrontendBudgetInvestment/src/components/Header.jsx
@@ -1,6 +1,7 @@
 export default function Header(){
     return(
         <div id="header">
+            <img src="/budgetInvestment-logo.png"/>
             <h1>Prowadź swoje oszczędności oraz kontroluj wydatki wraz z aplikacją Budget Ivestment</h1>
         </div>
     )
